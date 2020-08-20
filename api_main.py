fa9c97768c56f179cc4650762ef0030671c5df75
Ali Akolade <aliakolade@gmail.com>
2020-08-20 06:26:35 +0100

    Finish API on pythonanywhere

--- api_main.py
+++ api_main.py
@@ -1,22 +1,26 @@
-from flask import Flask, request
-import pandas as pd
 import pickle
+
+import pandas as pd
+from flask import Flask, request
 from numpy import array
+from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    result = 'Hello!'
+    result = 'Hello!\nWhat you passed isn\'t correct... O wrong nau...\nA lot of time was spent making this API so check your url and send the correct variables.\n\n  Yours truly, Bonus Salus\n✌'
     diabetes = str(request.args.get('diabetes'))
     cvd = str(request.args.get('cvd'))
     hypertension = str(request.args.get('hypertension'))
+    stress = str(request.args.get('stress'))
 
     if diabetes == '1':
-
         pregnancies = str(request.args.get('pregnancies'))
         glucose = str(request.args.get('glucose'))
         bloodpressure = str(request.args.get('bloodpressure'))
@@ -27,11 +31,14 @@ def hello_world():
         age = str(request.args.get('age'))
 
         dataset = pd.read_csv('/home/TheHealthDome2/Conditions/Data/Diabetes.csv')
-        col = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
+        col = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction',
+               'Age']
         X = dataset[list(col)].values
 
-        #test = [[6, 148, 72, 35, 155.54822, 33.6, 0.627, 50]]
-        test = [[float(pregnancies), float(glucose), float(bloodpressure), float(skinthickness), float(insulin), float(bmi), float(diabetespedigreefunction), float(age)]]
+        # test = [[6, 148, 72, 35, 155.54822, 33.6, 0.627, 50]]
+        test = [
+            [float(pregnancies), float(glucose), float(bloodpressure), float(skinthickness), float(insulin), float(bmi),
+             float(diabetespedigreefunction), float(age)]]
         sc = StandardScaler()
         X = sc.fit_transform(X)
         test = sc.transform(test)
@@ -62,10 +69,14 @@ def hello_world():
         df.isnull().values.any()
         df[df.ap_lo > df.ap_hi]
         df.sort_values('BMI')
-        df.drop(df[(df['height'] > df['height'].quantile(0.99)) | (df['height'] < df['height'].quantile(0.01))].index, inplace=True)
-        df.drop(df[(df['weight'] > df['weight'].quantile(0.99)) | (df['weight'] < df['weight'].quantile(0.01))].index, inplace=True)
-        df.drop(df[(df['ap_hi'] > df['ap_hi'].quantile(0.99)) | (df['ap_hi'] < df['ap_hi'].quantile(0.01))].index, inplace=True)
-        df.drop(df[(df['ap_lo'] > df['ap_lo'].quantile(0.99)) | (df['ap_lo'] < df['ap_lo'].quantile(0.01))].index, inplace=True)
+        df.drop(df[(df['height'] > df['height'].quantile(0.99)) | (df['height'] < df['height'].quantile(0.01))].index,
+                inplace=True)
+        df.drop(df[(df['weight'] > df['weight'].quantile(0.99)) | (df['weight'] < df['weight'].quantile(0.01))].index,
+                inplace=True)
+        df.drop(df[(df['ap_hi'] > df['ap_hi'].quantile(0.99)) | (df['ap_hi'] < df['ap_hi'].quantile(0.01))].index,
+                inplace=True)
+        df.drop(df[(df['ap_lo'] > df['ap_lo'].quantile(0.99)) | (df['ap_lo'] < df['ap_lo'].quantile(0.01))].index,
+                inplace=True)
         df = df[df.ap_hi >= df.ap_lo]
         df.drop(df[(df['BMI'] > df['BMI'].quantile(0.99)) | (df['BMI'] < df['BMI'].quantile(0.01))].index, inplace=True)
         values = array(df['cholesterol'])
@@ -95,21 +106,29 @@ def hello_world():
         cols = df.columns.drop(['cardio'])
         df.drop(df[df.duplicated(cols)].index, inplace=True)
         df['gender'].replace(2, 0, inplace=True)
-        down = df[(df.cardio == 1) & (df.active == 1) & (df.smoke == 0) & (df.gluc_1 == 1) & (df.BMI < 25) & (80 < df.ap_lo) & (df.ap_lo < 90) & (120 < df.ap_hi) & (df.ap_hi < 140) & (df.chol1 == 1)].index
+        down = df[(df.cardio == 1) & (df.active == 1) & (df.smoke == 0) & (df.gluc_1 == 1) & (df.BMI < 25) & (
+                    80 < df.ap_lo) & (df.ap_lo < 90) & (120 < df.ap_hi) & (df.ap_hi < 140) & (df.chol1 == 1)].index
         df.drop(down, inplace=True)
-        down2 = df[(df.cardio == 0) & (df.active == 0) & (df.gluc_3 == 1) & (df.BMI > 30) & ((90 < df.ap_lo) | (df.ap_lo < 80)) & ((140 < df.ap_hi) | (df.ap_hi < 120)) & (df.chol3 == 1)].index
+        down2 = df[(df.cardio == 0) & (df.active == 0) & (df.gluc_3 == 1) & (df.BMI > 30) & (
+                    (90 < df.ap_lo) | (df.ap_lo < 80)) & ((140 < df.ap_hi) | (df.ap_hi < 120)) & (df.chol3 == 1)].index
         df.drop(down2, inplace=True)
-        df[(df.cardio == 0) & (df.active == 0) & (df.BMI > 35) & ((90 < df.ap_lo) | (df.ap_lo < 80)) & ((140 < df.ap_hi) | (df.ap_hi < 120)) & ((df.chol3 == 1) | (df.gluc_3 == 1))]
+        df[(df.cardio == 0) & (df.active == 0) & (df.BMI > 35) & ((90 < df.ap_lo) | (df.ap_lo < 80)) & (
+                    (140 < df.ap_hi) | (df.ap_hi < 120)) & ((df.chol3 == 1) | (df.gluc_3 == 1))]
         df.drop(df[(df.BMI > 40) & (df.cardio == 0)].index, inplace=True)
         df.groupby('gender').count()
         down3 = df[(df.gluc_3 == 1) & (df.chol3 == 1) & (df.cardio == 0) & ((df.ap_hi > 140) | (df.ap_hi < 120))].index
         df.drop(down3, inplace=True)
-        dflow0 = df[(df.age < 45) & (df.active == 1) & (df.chol1 == 1) & (df.cardio == 0) & (df.smoke == 0) & (df.alco == 0) & (df.gluc_1 == 1) & (df.BMI <= 35)]
+        dflow0 = df[
+            (df.age < 45) & (df.active == 1) & (df.chol1 == 1) & (df.cardio == 0) & (df.smoke == 0) & (df.alco == 0) & (
+                        df.gluc_1 == 1) & (df.BMI <= 35)]
         dflow1 = df[(df.age < 45) & (df.active == 0) & (df.chol1 == 0) & (df.gluc_1 == 0)]
-        dfmid0 = df[(df.cardio == 0) & (df.age >= 45) & (df.age <= 58) & ((df.chol1 == 1) & (df.active == 1)) & (df.gluc_1 == 1) & (df.smoke == 0) & (df.alco == 0) & (df.BMI <= 30)]
+        dfmid0 = df[(df.cardio == 0) & (df.age >= 45) & (df.age <= 58) & ((df.chol1 == 1) & (df.active == 1)) & (
+                    df.gluc_1 == 1) & (df.smoke == 0) & (df.alco == 0) & (df.BMI <= 30)]
         dfmid1 = df[(df.cardio == 1) & (df.age >= 45) & (df.age <= 58) & ((df.chol1 == 0) | (df.active == 0))]
         dfhi1 = df[(df.age > 60) & (df.cardio == 1) & (df.chol1 == 0)]
-        dfhi0 = df[(df.age > 60) & (df.cardio == 0) & (df.chol1 == 1) & (df.smoke == 0) & (df.alco == 0) & (df.gluc_1 == 1) & (df.BMI <= 30)]
+        dfhi0 = df[
+            (df.age > 60) & (df.cardio == 0) & (df.chol1 == 1) & (df.smoke == 0) & (df.alco == 0) & (df.gluc_1 == 1) & (
+                        df.BMI <= 30)]
         final = dflow0.append([dflow1, dfmid0, dfmid1, dfhi0, dfhi1])
         bad = df[(df.cardio == 0) & (df.active == 0) & (df.chol1 == 0)]
         bad1 = df[(df.cardio == 1) & (df.active == 1) & (df.chol1 == 1)]
@@ -118,8 +137,10 @@ def hello_world():
         final = newfinal1
         predictors = final.drop(["cardio", 'BMI'], axis=1)
         target = final['cardio']
-        X_train, X_test, Y_train, Y_test = train_test_split(predictors, target, test_size=0.2, random_state=1, stratify=target)
-        X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=0.2, random_state=1, stratify=Y_train)
+        X_train, X_test, Y_train, Y_test = train_test_split(predictors, target, test_size=0.2, random_state=1,
+                                                            stratify=target)
+        X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=0.2, random_state=1,
+                                                          stratify=Y_train)
 
         sc_X = StandardScaler()
         X_train = sc_X.fit_transform(X_train)
@@ -152,11 +173,11 @@ def hello_world():
                 gluc2 = 0
                 gluc3 = 1
 
-            return [age, gender, height, weight, ap_hi, ap_lo, smoke, alco, active, chol1, chol2, chol3, gluc1, gluc2, gluc3]
+            return [age, gender, height, weight, ap_hi, ap_lo, smoke, alco, active, chol1, chol2, chol3, gluc1, gluc2,
+                    gluc3]
 
-        #test = get_input(56, 1, 166, 80, 160, 100, 3, 3, 1, 1, 3)
+        # test = get_input(56, 1, 166, 80, 160, 100, 3, 3, 1, 1, 3)
         test = get_input(age, gender, height, weight, ap_hi, ap_lo, chol, glucs, smoke, alco, active)
-
 
         X_test = sc_X.fit_transform([test])
 
@@ -168,8 +189,8 @@ def hello_world():
 
         sex = str(request.args.get('sex'))
         familyhxht = str(request.args.get('familyhxht'))
-        poor  = str(request.args.get('poor '))
-        bmi  = str(request.args.get('bmi '))
+        poor = str(request.args.get('poor '))
+        bmi = str(request.args.get('bmi '))
         wc = str(request.args.get('wc'))
         sbp = str(request.args.get('sbp'))
         dbp = str(request.args.get('dbp'))
@@ -177,10 +198,9 @@ def hello_world():
 
         filename = '/home/TheHealthDome2/Conditions/Models/Hypertension.pickle'
         tree = pickle.load(open(filename, 'rb'))
-        #example1 = [2, 2, 0, 24.143655, 92, 110, 80, 0, ]
-        example1 = [sex, familyhxht, poor , bmi , wc, sbp, dbp, htn]
+        # example1 = [2, 2, 0, 24.143655, 92, 110, 80, 0, ]
+        example1 = [sex, familyhxht, poor, bmi, wc, sbp, dbp, htn]
         example = pd.Series(data=example1, index=['Sex', 'FamilyHxHT', 'Poor ', 'BMI ', 'WC', 'SBP', 'DBP', 'HTN'])
-
 
         def classify_example(example, tree):
             question = list(tree.keys())[0]
@@ -203,5 +223,37 @@ def hello_world():
 
         results = int(classify_example(example, tree))
         result = [results]
+
+    if stress == '1':
+        ecg = str(request.args.get('ecg'))
+        emg = str(request.args.get('emg'))
+        foot_gsr = str(request.args.get('foot_gsr'))
+        hand_gsr = str(request.args.get('hand_gsr'))
+        hr = str(request.args.get('hr'))
+        resp = str(request.args.get('resp'))
+
+        df = pd.read_csv('/home/TheHealthDome2/Conditions/Data/Stress.csv', header=None)
+
+        df.columns = ['Target', 'ECG(mV)', 'EMG(mV)', 'Foot GSR(mV)', 'Hand GSR(mV)', 'HR(bpm)', 'RESP(mV)']
+        X_train, X_test, y_train, y_test = train_test_split(
+            df[['ECG(mV)', 'EMG(mV)', 'Foot GSR(mV)', 'Hand GSR(mV)', 'HR(bpm)', 'RESP(mV)']], df['Target'],
+            test_size=0.30, random_state=12345)
+        filename = "/home/TheHealthDome2/Conditions/Models/Stress.pickle"
+
+        minmax_scale = preprocessing.MinMaxScaler().fit(
+            df[['ECG(mV)', 'EMG(mV)', 'Foot GSR(mV)', 'Hand GSR(mV)', 'HR(bpm)', 'RESP(mV)']])
+        df_minmax = minmax_scale.transform(
+            df[['ECG(mV)', 'EMG(mV)', 'Foot GSR(mV)', 'Hand GSR(mV)', 'HR(bpm)', 'RESP(mV)']])
+        X_train_norm, X_test_norm, y_train_norm, y_test_norm = train_test_split(df_minmax, df['Target'], test_size=0.30,
+                                                                                random_state=42)
+
+        knn_norm = KNeighborsClassifier(n_neighbors=5)
+        filename = '/home/TheHealthDome2/Conditions/Models/Stress.pickle'
+        knn_norm = pickle.load(open(filename, 'rb'))
+
+        # pred_data_norm = minmax_scale.transform([[0.001,0.931,5.91,19.773,99.065,35.59]])
+        pred_data_norm = minmax_scale.transform([[ecg, emg, foot_gsr, hand_gsr, hr, resp]])
+        pred = knn_norm.predict(pred_data_norm)
+        result = str(pred)
 
     return str(result)
